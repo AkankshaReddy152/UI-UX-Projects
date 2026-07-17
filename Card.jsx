63a50a90f9cd1579c8721a7abbb058ed4807ee96
AkankshaReddy152 <akankshareddy152@gmail.com>
2026-07-17 11:15:52 +0530

    Insta_Post

--- Card.jsx
+++ Card.jsx
@@ -1,16 +1,15 @@
 import "./Card.css"
-export default function Card({title,description,age,wikilink,imglink}){
-return(
-   <>
-  
-   <div className="card">
-    <img src={imglink}/>
-    <h2>{title}</h2>
-    <p>{description}</p>
-    <h3>Age:{age}</h3>
-    <a href={wikilink}>View Wiki</a>
-
-   </div>
-   </>
+import { useState } from "react";
+export default function Card(){
+      const [liked, setLiked] = useState(false);
+    return(
+        <> 
+            <div className="card">
+                <img src="https://tse2.mm.bing.net/th/id/OIP.G37tgeQqSNt7v2oPfj9ltQHaE7?r=0&rs=1&pid=ImgDetMain&o=7&rm=3"/>
+                <h2>Nature Post</h2>
+                <button onClick={() => setLiked(!liked)}>{liked ? "❤️ Unlike" : "🩷 Like"}</button>
+            </div>
+        </>
     );
+
 }
